a62129ba12e47e092af327867d2636a340bf51c5
ilman09 <84706542+ilman09@users.noreply.github.com>
2021-11-30 17:42:15 +0700

    Update welcome_message.php

--- welcome_message.php
+++ welcome_message.php
@@ -1,5 +1,5 @@
 <div class="container">
     <h1 class="mt-5 ">Welcome to Dukuns kopi</h1>
-    <p class="lead mb-5">program ini dibuat untuk menginput data barang milik dukun kopi <br/> <small>- mr</small></p>
-    <p>buat list item baru klik <a href="http://localhost/percobaan/create.php">disini</a>.</p>
+    <p class="lead mb-5">program ini dibuat untuk menginput data barang milik dukuns kopi <br/> <small>- mr</small></p>
+    
 </div>
